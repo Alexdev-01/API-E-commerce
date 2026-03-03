c908010afbe88925f3977378228ad7743f5ff609
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-03-03 17:59:23 +0100

    Add files via upload

--- API_Gestion_Productos_Y_Pedidos/src/main/java/com/tiendaonline/gestion/service/impl/CategoriaServiceImpl.java
+++ API_Gestion_Productos_Y_Pedidos/src/main/java/com/tiendaonline/gestion/service/impl/CategoriaServiceImpl.java
@@ -1,1 +1,27 @@
+package com.tiendaonline.gestion.service.impl;
 
+import java.util.List;
+
+import com.tiendaonline.gestion.model.Categoria;
+import com.tiendaonline.gestion.repository.CategoriaRepository;
+
+@Service	// Marca esta clase como un componente de servicio en Spring
+public class CategoriaServiceImpl {
+
+	private final CategoriaRepository categoriaRepository;
+
+	public CategoriaServiceImpl(CategoriaRepository categoriaRepository) {
+		this.categoriaRepository = categoriaRepository;
+	}
+
+	@Override
+	public Categoria crearCategoria(Categoria categoria) {
+		return categoriaRepository.save(categoria);
+	}
+
+	@Override
+	public List<Categoria> listarTodas() {
+		return categoriaRepository.findAll();
+	}
+
+}
