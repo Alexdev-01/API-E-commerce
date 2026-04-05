71add9e8d80ed99239ba66002242023f25489bd7
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-04-05 10:47:34 +0200

    Add files via upload

--- API_Gestion_Productos_Y_Pedidos/src/main/resources/BBDD.sql
+++ API_Gestion_Productos_Y_Pedidos/src/main/resources/BBDD.sql
@@ -1,1 +1,75 @@
 
+<!--TABLAS-->
+CREATE TABLE usuarios (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    rol VARCHAR(20) NOT NULL
+);
+
+
+CREATE TABLE categorias (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    descripcion VARCHAR(255)
+);
+
+
+CREATE TABLE productos (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    descripcion VARCHAR(255),
+    precio DECIMAL(10,2) NOT NULL,
+    stock INT NOT NULL,
+    activo BOOLEAN DEFAULT TRUE,
+
+    categoria_id BIGINT,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+);
+
+
+CREATE TABLE productos (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    descripcion VARCHAR(255),
+    precio DECIMAL(10,2) NOT NULL,
+    stock INT NOT NULL,
+    activo BOOLEAN DEFAULT TRUE,
+
+    categoria_id BIGINT,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+);
+
+
+CREATE TABLE pedidos (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id BIGINT,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    total DECIMAL(10,2),
+
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
+
+
+CREATE TABLE detalle_pedido (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    pedido_id BIGINT,
+    producto_id BIGINT,
+    cantidad INT NOT NULL,
+    precio_unitario DECIMAL(10,2),
+    subtotal DECIMAL(10,2),
+
+    FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
+    FOREIGN KEY (producto_id) REFERENCES productos(id)
+);
+
+
+<!--DATOS DE PRUEBA-->
+INSERT INTO categorias (nombre, descripcion) VALUES
+('Electrónica', 'Productos electrónicos'),
+('Ropa', 'Ropa y accesorios');
+
+INSERT INTO productos (nombre, descripcion, precio, stock, categoria_id) VALUES
+('Laptop', 'Laptop potente', 1200, 10, 1),
+('Camiseta', 'Camiseta básica', 20, 50, 2);
